f0b56b002a8b361e401a3160acfd9597336cb8f3
Abhi Velaga <abhinav.velaga@gmail.com>
2021-11-26 03:01:22 -0600

    go t it working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+import Papa from 'papaparse';
 
 function App() {
+  const [parsedCsvData, setParsedCsvData] = useState([]);
+
+  const parseFile = file => {
+    Papa.parse(file, {
+      header: true,
+      complete: results => {
+        setParsedCsvData(results.data);
+      },
+    });
+  };
+
+  console.log(parsedCsvData);
+
+  const onDrop = useCallback(acceptedFiles => {
+    if (acceptedFiles.length) {
+      parseFile(acceptedFiles[0]);
+    }
+  }, []);
+
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({
+    onDrop,
+    accept: 'text/csv',
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        {...getRootProps({
+          className: `dropzone 
+          ${isDragAccept && 'dropzoneAccept'} 
+          ${isDragReject && 'dropzoneReject'}`,
+        })}
+      >
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>
+
+          {parsedCsvData &&
+            parsedCsvData.map((parsedData, index) => (
+              <div key={index}>{parsedData['Description']}</div>
+            ))}
+
+
     </div>
   );
 }
